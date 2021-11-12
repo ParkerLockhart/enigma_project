f2f8120b25b4a57bbcf2eec05793b14cf9510a9c
Parker <parkerlockhart42@gmail.com>
2021-11-11 20:51:28 -0600

    Test: initialize

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,1 +1,13 @@
 require './lib/enigma'
+
+RSpec.describe Enigma do
+  before(:each) do
+    @enigma = Enigma.new
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@enigma).to be_instance_of(Enigma)
+    end
+  end
+end
