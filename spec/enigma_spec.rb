16f693e3f683f0abc576567391907b3676df6dab
Parker <parkerlockhart42@gmail.com>
2021-11-11 21:28:57 -0600

    Test: encrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Enigma do
       expect(@enigma).to be_instance_of(Enigma)
     end
   end
+
+  describe '#encrypt' do
+    it 'encrypts a message' do
+      expect(@enigma.encypt).to be_instance_of(Hash)
+      expect(@enigma.encrypt.keys).to eq([:encryption, :key, :date])
+    end
+  end
 end
