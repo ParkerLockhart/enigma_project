fd512e63cfd66bf3a1e07632be34d43a348fca90
Parker <parkerlockhart42@gmail.com>
2021-11-12 00:44:05 -0600

    Add offset_generator method

--- lib/scrambler.rb
+++ lib/scrambler.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module Scrambler
 
   def key_generator
@@ -7,4 +9,15 @@ module Scrambler
     end
     keys.to_a.sample
   end
+
+  def offset_generator(date)
+    date = date.strftime("%m%d%Y").to_i
+    sq = (date * date).to_s
+    four = sq.slice(-4, 4)
+    offset = {"A" => four.slice(0), "B" => four.slice(1), "C" => four.slice(2), "D" => four.slice(3)}
+    offset.transform_values! do |v|
+      v.to_i
+    end
+  end
+
 end
