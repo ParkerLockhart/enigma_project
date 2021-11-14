17b5640ec2f42505beabb4b55b746a8e65df0d04
Parker <parkerlockhart42@gmail.com>
2021-11-13 18:09:41 -0600

    Add greeting method

--- lib/session.rb
+++ lib/session.rb
@@ -15,4 +15,18 @@ class Session
     puts " "*3
     greeting
   end
+
+  def greeting
+    if @time.hour.between?(4, 11)
+      puts "Good morning!"
+    elsif @time.hour.between?(12, 16)
+      puts "Good afternoon!"
+    elsif @time.hour.between?(17, 23)
+      puts "Good evening!"
+    elsif @time.hour.between?(0, 3)
+      puts "Hello there, night owl!"
+    end
+    puts "It is currently #{@time.strftime("%A, %B %d %Y at %I:%M %p")}."
+    encrypt_or_decrypt
+  end
 end
