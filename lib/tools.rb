5797a9cd80026e39dda6bd33ac6ab9b0e8c5ace6
Parker <parkerlockhart42@gmail.com>
2021-11-14 15:14:18 -0600

    Add offset_generator to tools module

--- lib/tools.rb
+++ lib/tools.rb
@@ -9,4 +9,18 @@ module Tools
     end
     keys.to_a.sample
   end
-end 
+
+  def offset_generator(date)
+    if date.class == Date
+      date = date.strftime("%m%d%Y").to_i
+    elsif date.class == String
+      date = date.to_i
+    end
+    sq = (date * date).to_s
+    four = sq.slice(-4, 4)
+    offset = {"A" => four.slice(0), "B" => four.slice(1), "C" => four.slice(2), "D" => four.slice(3)}
+    offset.transform_values! do |v|
+      v.to_i
+    end
+  end
+end
