acbdd531625f84d70cc7310360338339365b6bc6
Parker <parkerlockhart42@gmail.com>
2021-11-14 15:14:00 -0600

    Test: tools module offset_generator

--- spec/tools_spec.rb
+++ spec/tools_spec.rb
@@ -13,4 +13,11 @@ describe Tools do
       expect(a_class.key_generator.to_s.length).to eq(5)
     end
   end
+
+  describe '#offset_generator' do
+    it 'generates offset from date' do
+      expect(a_class.offset_generator(Date.today)).to be_instance_of(Hash)
+      expect(a_class.offset_generator(Date.today).keys).to eq(["A", "B", "C", "D"])
+    end
+  end
 end
