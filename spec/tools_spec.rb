e6644b87cb1f2d4437bc83e17c33640f87ee5f61
Parker <parkerlockhart42@gmail.com>
2021-11-14 18:56:02 -0600

    Add shift_generator and shifts tests to tools module, create date_fixer test

--- spec/tools_spec.rb
+++ spec/tools_spec.rb
@@ -1,4 +1,5 @@
 require './lib/tools'
+require 'date'
 
 describe Tools do
   let(:a_class) do
@@ -14,10 +15,50 @@ describe Tools do
     end
   end
 
+  describe '#date_fixer' do
+    it 'takes in date and outputs in integer format' do
+      expect(a_class.date_fixer(Date.today)).to be_instance_of(Integer)
+      expect(a_class.date_fixer("08041995")).to be_instance_of(Integer)
+    end
+  end
+
   describe '#offset_generator' do
     it 'generates offset from date' do
       expect(a_class.offset_generator(Date.today)).to be_instance_of(Hash)
       expect(a_class.offset_generator(Date.today).keys).to eq(["A", "B", "C", "D"])
+    end
+  end
+
+  describe '#shift_generator' do
+    it 'generates shift from key and offset' do
+      key = "02715"
+      offset = {"A" => 1, "B" => 0, "C" => 2, "D" => 5}
+      expect(a_class.shift_generator(key, offset)).to be_instance_of(Hash)
+      expect(a_class.shift_generator(key, offset)).to eq({"A" => 3, "B" => 27, "C" => 73, "D" => 20})
+    end
+  end
+
+  describe '#shift_a' do
+    it 'creates an array of numbers to use for shift A' do
+      expect(a_class.shift_a).to be_instance_of(Array)
+    end
+  end
+
+  describe '#shift_b' do
+    it 'creates an array of numbers to use for shift B' do
+      expect(a_class.shift_b).to be_instance_of(Array)
+    end
+  end
+
+  describe '#shift_c' do
+    it 'creates an array of numbers to use for shift C' do
+      expect(a_class.shift_c).to be_instance_of(Array)
+    end
+  end
+
+  describe '#shift_d' do
+    it 'creates an array of numbers to use for shift D' do
+      expect(a_class.shift_d).to be_instance_of(Array)
     end
   end
 end
